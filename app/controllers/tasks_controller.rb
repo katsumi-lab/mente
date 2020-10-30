2580eb3d5fda89e712d76a868f2ddf793003d5b4
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-30 13:03:47 +0900

    編集機能のテスト実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,12 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path, notice:"「#{task.name}」を更新しました。"
   end
 
   private
