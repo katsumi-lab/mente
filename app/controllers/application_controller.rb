717afcb91ca24c2a34417cd3ec338558979b482e
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-03 11:24:01 +0900

    TOPページからログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana]
-    )
+      :sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
-  resources :users, only: [:edit, :update]
-
+  # resources :users, only: [:edit, :update]
+  
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to action: :index
+  #   else
+  #     render :new
+  #   end
+  # end
+  def create
+    @user = User.create(user_params)
+  end
+  
   def edit
   end
+
+  private
+  def user_params
+    params.require(:user).permit(last_name: last_name, first_name:first_name)
+  end
+  
 end
