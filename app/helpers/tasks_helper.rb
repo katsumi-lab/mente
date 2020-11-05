66151eb15a27c920a3ad9813a77c7ee3e5d62913
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-05 19:14:50 +0900

    期限の差分による表示変更、日本語表記

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,28 @@
 module TasksHelper
+
+  def deadline_count(task)
+    today = Date.today
+    deadline = task.deadline
+    countdown = (deadline-today).to_i
+    if countdown == 0
+      "今日がお手入れの目安の日です！"
+    elsif countdown.between?(1, 3)
+      "お手入れの目安まで残り#{countdown}日"
+    else
+      "お手入れの目安日:#{l task.deadline}"
+    end
+  end
+
+  def deadline_style(task)
+    today = Date.today
+    deadline = task.deadline
+    countdown = (deadline - today).numerator
+    if countdown == 0
+      "dl-style1"
+    elsif countdown.between?(1, 3)
+      "dl-style2"
+    else
+      "dl-style3"
+    end
+  end
 end
