1836399a5d0ad1ad8ae29d74607dd498ec72485b
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-10 17:47:18 +0900

    ユーザー編集に関連するビューの修正

--- db/migrate/20201029053927_create_tasks.rb
+++ db/migrate/20201029053927_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :room
-      t.string :item
+      t.string :room      ,null: false
+      t.string :item      ,null: false
       t.text :description
       t.timestamps
     end
