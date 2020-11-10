0f937b6daee4ebcfe504ad676515963d6284e34d
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-10 22:39:29 +0900

    taskとuserのバリデーション記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,17 @@
 class Task < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to_active_hash :floor_category
+  belongs_to_active_hash :room_category
+  belongs_to_active_hash :item_category
+  belongs_to_active_hash :mentenance_category
+  belongs_to_active_hash :description_category
   
+  with_options presence: true do
+    validates :floor
+    validates :room
+    validates :item
+    validates :mentenance
+    validates :deadline
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true
+  validates :password, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+  with_options presence: true do
+    with_options format: { with: /^[ぁ-んァ-ン一-龥]/, multiline: true } do
+      validates :family_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, multiline: true } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+  end
 end
