156979dcb7de2219f17b4b57a4fadfb7fad5b3f9
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-16 21:58:46 +0900

    目安日が過ぎている条件分岐をTaskshelperに追記

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -8,21 +8,11 @@ module TasksHelper
       "今日がお手入れの目安の日です！"
     elsif countdown.between?(1, 3)
       "お手入れの目安まで残り#{countdown}日"
+    elsif countdown < 0
+      "目安の日を過ぎています"
     else
       "お手入れの目安日:#{l task.deadline}"
     end
   end
 
-  def deadline_style(task)
-    today = Date.today
-    deadline = task.deadline
-    countdown = (deadline - today).numerator
-    if countdown == 0
-      "dl-style1"
-    elsif countdown.between?(1, 3)
-      "dl-style2"
-    else
-      "dl-style3"
-    end
-  end
 end
