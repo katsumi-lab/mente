728f862f5ff3eedd99aa0bb0cd82961cce19d292
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-17 22:13:59 +0900

    _idの追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,10 +8,10 @@ class Task < ApplicationRecord
 
   
   with_options presence: true do
-    validates :floor
-    validates :room
-    validates :item
-    validates :mentenance
+    validates :floor_id
+    validates :room_id
+    validates :item_id
+    validates :mentenance_id
     validates :deadline
   end
 end
