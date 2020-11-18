fb2158747e4cd61bc09d2951af3ff39acadfacd6
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-18 13:09:35 +0900

    application.rbの記述変更

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 module Mente
   class Application < Rails::Application
@@ -13,7 +13,7 @@ module Mente
     config.load_defaults 6.0
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
-    config.action_dispatch.default_headers['Referrer-Policy'] = 'no-referrer-when-downgrade'
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
