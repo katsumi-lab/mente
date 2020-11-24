394af4c01abd7e88836da7c6f7a6121d76985a67
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-24 11:11:36 +0900

    新規登録のテストコード

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    "" = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'Last_name', with: "".last_name
+      fill_in 'First_name', with: "".first_name
+      fill_in 'Last_name_kana', with: "".last_name_kana
+      fill_in 'First_name_kana', with: "".first_name_kana
+      fill_in 'Email', with: "".email
+      fill_in 'Email_confirmation', with: "".email_confirmation
+      fill_in 'Password', with: "".password
+      fill_in 'Password_sonfirmation', with: "".password_confirmation
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移する
+      expect(current_path).to eq root_path
+      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      expect(
+        find(".user_nav").find("span").hover
+      ).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'Last_name', with: ""
+      fill_in 'First_name', with: ""
+      fill_in 'Last_name_kana', with: ""
+      fill_in 'First_name_kana', with: ""
+      fill_in 'Email', with: ""
+      fill_in 'Email_confirmation', with: ""
+      fill_in 'Password', with: ""
+      fill_in 'Password_sonfirmation', with: ""
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
+    end
+  end
+end
